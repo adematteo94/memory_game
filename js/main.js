a58b5238583fa2a12268c1ae8cb1756c38bef559
Anthony DeMatteo <adematteo94@gmail.com>
2017-05-17 14:40:52 -0700

    added game methods

--- js/main.js
+++ js/main.js
@@ -1,10 +1,51 @@
 
-var cards = ["Joker", "Joker", "Ace", "Ace", "Jack", "Jack", "Queen", "Queen", "King", "King", "Blank", "Blank"];
+var cards = [
+{
+		rank: "King",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	},
+	{
+		rank: "King",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "Queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "Queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "King",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	},
+	{
+		rank: "King",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "Queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "Queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	}
+];
 
 var cardsInPlay = [];
 
 var checkForMatch = function() {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
+	if (cardsInPlay[0].rank === cardsInPlay[1].rank && cardsInPlay[0].suit === cardsInPlay[1].suit) {
 		alert("You found a match!");
 	} else {
 		alert("Try again.");
@@ -13,19 +54,19 @@ var checkForMatch = function() {
 
 var flipCard = function(cardId) {
 	
-	console.log("You flipped the "  + cards[cardId]);
+	console.log("You flipped the "  + cards[cardId].rank);
 
 	cardsInPlay.push(cards[cardId]);
 
-	checkForMatch();	
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+
 	}
 
 
 flipCard(0);
-flipCard(5);
-
-
-
+flipCard(4);
+checkForMatch();
 
 
 
