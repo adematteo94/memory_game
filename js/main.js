ca0df1a4f165263e96bce82f5183d0307754562b
Anthony DeMatteo <adematteo94@gmail.com>
2017-06-17 09:20:00 -0700

    nearly complete, looking to finish the card flip back function

--- js/main.js
+++ js/main.js
@@ -5,42 +5,50 @@ var cards = [
 {
 		rank: "King",
 		suit: "diamonds",
-		cardImage: "images/king-of-diamonds.png"
+		cardImage: "images/king-of-diamonds.png",
+		backImage: "images/back.png"
 	},
 	{
 		rank: "King",
 		suit: "hearts",
-		cardImage: "images/king-of-hearts.png"
+		cardImage: "images/king-of-hearts.png",
+		backImage: "images/back.png"
 	},
 	{
 		rank: "Queen",
 		suit: "diamonds",
-		cardImage: "images/queen-of-diamonds.png"
+		cardImage: "images/queen-of-diamonds.png",
+		backImage: "images/back.png"
 	},
 	{
 		rank: "Queen",
 		suit: "hearts",
-		cardImage: "images/queen-of-hearts.png"
+		cardImage: "images/queen-of-hearts.png",
+		backImage: "images/back.png"
 	},
 	{
 		rank: "King",
 		suit: "diamonds",
-		cardImage: "images/king-of-diamonds.png"
+		cardImage: "images/king-of-diamonds.png",
+		backImage: "images/back.png"
 	},
 	{
 		rank: "King",
 		suit: "hearts",
-		cardImage: "images/king-of-hearts.png"
+		cardImage: "images/king-of-hearts.png",
+		backImage: "images/back.png"
 	},
 	{
 		rank: "Queen",
 		suit: "diamonds",
-		cardImage: "images/queen-of-diamonds.png"
+		cardImage: "images/queen-of-diamonds.png",
+		backImage: "images/back.png"
 	},
 	{
 		rank: "Queen",
 		suit: "hearts",
-		cardImage: "images/queen-of-hearts.png"
+		cardImage: "images/queen-of-hearts.png",
+		backImage: "images/back.png"
 	}
 ];
 
@@ -52,13 +60,6 @@ var scoreUpdate = function() {
 	var points = document.getElementById('points');
 	points.textContent = score;
 };
-
-//Trying to figure out how to not reset the points
-//var keepScore = function () {
-	//var currentScore = document.getElementById('points');
-	//currentScore = score + score;
-	//return currentScore;
-//};
 
 var shuffle = function(deck) {
     var counter = deck.length;
@@ -72,6 +73,37 @@ var shuffle = function(deck) {
     return deck;
 };
 
+var resetUnmatchedCards = function() {
+	//Looking to select the two most recently flipped cards, replace their image with the image of the back of the card.
+	cardsInPlay.forEach(function(image) {
+		var cardFront = document.getElementsByName('in');
+		cardFront.setAttribute('src', 'images/back.png');
+		console.log(cardId);
+
+	})
+
+
+	//for (var i = 0; i < cardsInPlay.length; i++) {
+		//having trouble selecting the flipped card and changing it back to its original state (tha back of the card).
+		//How do I select the cards that are in cardsInPlay?
+
+		//this is what I have experimented with, hoping to grab the cards that get the name attribute 'in' from the flip card function.
+		//var card = document.getElementById('game-board');
+		//var cardFront = card.getElementsByName('in')[i];
+		//cardFront.setAttribute('src', cardsInPlay[i].backImage);
+
+
+		//this works partially but only grabs the first two cards because I am selecting the img tag, and only the frist 2 as noted with i.
+		/*var card = document.getElementById('game-board');
+		var cardFront = card.getElementsByTagName('img')[i];
+		//Right now I can only select the first two cards in the div, which I dont want. Need to flip back any card that someone flips. 
+		cardFront.setAttribute('src', cardsInPlay[i].backImage);*/
+		
+	//}
+	cardsInPlay.pop(cards[cardId]);
+	cardsInPlay.pop(cards[cardId]);
+};
+
 var checkForMatch = function() {
 	if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0].rank === cardsInPlay[1].rank && cardsInPlay[0].suit === cardsInPlay[1].suit) {
@@ -80,19 +112,18 @@ var checkForMatch = function() {
 		cardsInPlay.pop(cards[cardId]);
 		cardsInPlay.pop(cards[cardId]);
 		} else {
-		cardsInPlay.pop(cards[cardId]);
-		cardsInPlay.pop(cards[cardId]);
+		setTimeout(function() { resetUnmatchedCards(); }, 750);
 		}
 	}	
 };
 
 var flipCard = function() {
-	var cardId = this.getAttribute('data-id');
+	 
+ 	var cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId]);
-	console.log("User flipped " + cards[cardId].rank);
-	console.log(cards[cardId].suit);
-	console.log(cards[cardId].cardImage);
 	this.setAttribute('src', cards[cardId].cardImage);
+	this.setAttribute('name', 'in');
+	console.log("user clicked the " + cards[cardId].rank + " of " + cards[cardId].suit);	
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}	
@@ -101,6 +132,7 @@ var flipCard = function() {
 var createBoard = function () {
 	shuffle(cards);
 	for (var i = 0; i < cards.length; i++) {
+		console.log(i)
 		var cardElement = document.createElement("img");
 		cardElement.setAttribute('src', 'images/back.png');
 		cardElement.setAttribute('data-id', i);
@@ -116,10 +148,6 @@ var removeBoard = function () {
 var resetBoard = function () {
 	removeBoard();
 	createBoard();
-};
-
-var resetGame = function () {
-	
 };
 
 createBoard();
